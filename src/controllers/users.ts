029a1bc3493893dd460435850de13b8f610f1ae6
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-03-24 00:05:47 +0530

    add: Update user route

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,13 +1,45 @@
 import express from "express";
 
-import { getUsers } from "../db/users";
+import { deleteUserById, getUsers, getUserById } from "../db/users";
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
         const users = await getUsers();
-
+        
+        return res.status(200).json(users);
 
     } catch(error){
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
+export const deleteUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params;
+
+        // Response after deleting the user is stored inside deletedUser 
+        const deletedUser = deleteUserById(id);
+        return res.json(deletedUser);
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
+export const updateUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params;
+        const { username } = req.body;
+
+        if(!username){
+            return res.sendStatus(400);
+        }
+        const user = getUserById(id);
+        await user.save(); // Generated error : Property save DNE
+
+        return res.status(200).json(user).end();
+    } catch (error) {
         console.log(error);
         return res.sendStatus(400);
     }
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -3,6 +3,32 @@ import { get, merge }  from "lodash";
 
 import { getUserBySessionToken } from "../db/users";
 
+// Account deletions to be allowed only if the account owner is requesting 
+// for it. Otherwise deletion should not be allowed.
+export const isOwner = async (req: express.Request, 
+    res: express.Response, next: express.NextFunction) => {
+    
+    try {
+        const { id } = req.params;
+        const currentUserId = get(req, 'identity._id') as string;
+        
+        if (!currentUserId) {
+            return res.sendStatus(403);
+        }
+
+        if(currentUserId.toString() !== id){
+            return res.sendStatus(403);
+        }
+
+        next();
+
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
+// Authentication middleware
 export const isAuthenticated = async (req: express.Request, 
     res: express.Response, next: express.NextFunction) => {
 

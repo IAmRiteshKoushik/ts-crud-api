5f658c2f1579cf9346140172881934e258c34a49
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-03-23 23:43:35 +0530

    complete: Login controller, cookies

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -3,6 +3,46 @@ import express from 'express';
 import { getUserByEmail, createUser } from "../db/users";
 import { random, authentication } from "../helpers";
 
+export const login = async (req: express.Request, res: express.Response) => {
+    try {
+        const { email, password } = req.body;
+
+        // Check for availability of email and password
+        if(!email || !password ){
+            return res.sendStatus(400);
+        }
+
+        // Should have been an async function, but the action has been
+        // written synchronously. Would be better to improve it later on
+        // the select() statements are very important because you are forcefully
+        // bringing out the salt and the password ... which by default are set 
+        // to false during selection
+        const user = getUserByEmail(email).select("+authentication.salt +authentication.password");
+        if(!user) {
+            return res.sendStatus(400);
+        }
+
+        const expectedHash = authentication(user.authentication.salt, password);
+        if (user.authentication.password != expectedHash){
+            return res.sendStatus(403); // Forbidden : Password invalid
+        }
+
+        const salt = random();
+        user.authentication.sessionToken = authentication(salt, user._id.toString());
+        await user.save();
+
+        // Cookie based authentication for user sessions (look into it)
+        res.cookie('ANTIONIO-AUTH', user.authentication.sessionToken, {
+            domain: "localhost",
+        });
+        return res.status(200).json(user).end();
+
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
 export const register = async (req: express.Request, res: express.Response) => {
     try{
         const { email, password, username } = req.body;
@@ -12,14 +52,16 @@ export const register = async (req: express.Request, res: express.Response) => {
             return res.sendStatus(400);
         }
 
+        // Checking if user already exists
         const existingUser = await getUserByEmail(email);
-
         if(existingUser){
-            return res.sendStatus(email);
+            return res.sendStatus(400);
         }
 
-        const salt = random();
-        const user = await createUser({
+        const salt = random(); // Generates 128 bytes of data randomly (string)
+        // It is better if createUser was an async function and we could use 
+        // await syntax with it.
+        const user = createUser({
             email,
             username,
             authentication: {
@@ -27,7 +69,6 @@ export const register = async (req: express.Request, res: express.Response) => {
                 password: authentication(salt, password),
             },
         });
-
         return res.sendStatus(200).json(user).end();
     } catch (error) {
         console.log(error);

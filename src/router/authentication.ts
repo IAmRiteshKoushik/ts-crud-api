39b2c274619cfdf22c326d74b25fba593ada0342
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-03-23 23:44:37 +0530

    add: Login route

--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 
-import { register } from "../controllers/authentication";
+import { login, register } from "../controllers/authentication";
 
 export default (router: express.Router) => {
     router.post('/auth/register', register);
-    // Hits the register controller
+    router.post('/auth/login', login);
 };
 
 

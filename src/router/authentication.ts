1c58fbe1c183328dcf8763d31a00e467c81665e5
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-03-23 23:25:22 +0530

    done: Routers

--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -4,6 +4,7 @@ import { register } from "../controllers/authentication";
 
 export default (router: express.Router) => {
     router.post('/auth/register', register);
+    // Hits the register controller
 };
 
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
-import express from 'express';
+import express from "express";
 
-import authentication from './authentication';
+import authentication from "./authentication";
+import users from "./users";
 
 const router = express.Router();
 
 export default(): express.Router => {
-    authentication(router);
+    authentication(router); // Adding an authentication layer
+    users(router) // Getting users only if the authentication layer works
     return router;
 }
 
